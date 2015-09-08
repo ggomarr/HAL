747b7c6823951b336abc1ff011ccaf702f85fb73
Gonzalo <ggomarr@gmail.com>
2015-09-08 11:46:20 +0300

    First complete bot.

--- src/root/create_bot.py
+++ src/root/create_bot.py
@@ -6,23 +6,33 @@ Created on Sep 8, 2015
 
 # Parameters
 
-alice_lib_root = '/home/ggomarr/eclipse/workspace/PyAIML/alice/'
+alice_lib_root = '/home/ggomarr/eclipse/workspace/PyAIML/01 Alice/'
+
+alice_brain = 'brain/alice.brain'
 
 alice_libs = [ 'reduction?.safe.aiml',
                'reductions.update.aiml',
-               'mp?.aiml' ]
+               'mp?.aiml',
+               '*.aiml' ]
 
-#alice_libs = [ 'reduction?.safe.aiml',
-#               'reductions.update.aiml',
-#               'mp?.aiml',
-#               '*.aiml' ]
+end_cue = 'BYE'
 
 # End parameters
 
 import aiml
+import os.path
 
 alice = aiml.Kernel()
 
-for alice_lib in alice_libs:
-    alice.learn(alice_lib_root + alice_lib)
+if os.path.isfile(alice_lib_root + alice_brain):
+    alice.loadBrain(alice_lib_root + alice_brain)
+else:
+    for alice_lib in alice_libs:
+        alice.learn(alice_lib_root + alice_lib)
     
+input_txt=''
+while input_txt != end_cue:
+    input_txt = raw_input('?> ')
+    print alice.respond(input_txt)
+    
+alice.saveBrain(alice_lib_root + alice_brain)

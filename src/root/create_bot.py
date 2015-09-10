a654e8c5498e679e1dc48f8a16bce2cd4de9ecfb
Gonzalo <ggomarr@gmail.com>
2015-09-10 14:00:32 +0300

    First working version with audio input and output.

--- src/root/create_bot.py
+++ src/root/create_bot.py
@@ -15,12 +15,19 @@ alice_libs = [ 'reduction?.safe.aiml',
                'mp?.aiml',
                '*.aiml' ]
 
-end_cue = 'BYE'
+end_cue = 'goodbye'
+
+listen = True
+speak  = True
+
+min_energy_threshold = 5000
 
 # End parameters
 
 import aiml
 import os.path
+import speech_recognition
+import pyttsx
 
 alice = aiml.Kernel()
 
@@ -29,10 +36,39 @@ if os.path.isfile(alice_lib_root + alice_brain):
 else:
     for alice_lib in alice_libs:
         alice.learn(alice_lib_root + alice_lib)
-    
+
+if listen:
+    listenah = speech_recognition.Recognizer()
+    mic = speech_recognition.Microphone()
+
+    print("A moment of silence, please...")
+    with mic as source:
+        listenah.adjust_for_ambient_noise(source)
+        listenah.energy_threshold=max(listenah.energy_threshold,min_energy_threshold)
+        pass
+    print("Set minimum energy threshold to {}".format(listenah.energy_threshold))
+
+
 input_txt=''
 while input_txt != end_cue:
-    input_txt = raw_input('?> ')
-    print alice.respond(input_txt)
-    
+    if listen:    
+        print '[Waiting for you to speak]'
+        with mic as source:
+            utterance = listenah.listen(source)
+            pass
+        input_txt = listenah.recognize_google(utterance)
+        print '?> ' + input_txt
+    else:
+        print '[Waiting for you to type]'
+        input_txt = raw_input('?> ')
+        
+    answer_txt = alice.respond(input_txt)
+    print answer_txt
+
+    if speak:
+        speaker = pyttsx.init()
+        speaker.say(answer_txt)
+        speaker.runAndWait()
+        del speaker
+
 alice.saveBrain(alice_lib_root + alice_brain)
